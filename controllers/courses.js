bb952c765624caccb3e2f6b57c672f719bcea3fc
massimilianoBaruffato <67097584+massimilianoBaruffato@users.noreply.github.com>
2020-06-29 13:45:45 +0200

    secondo
    secondo

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,12 +7,36 @@ const { response } = require("express");
 const router= express.Router();
 const CourseModel=mongoose.model("Course")
 var course= new CourseModel();
-course.courseName="NodeJS";
-course.courseId="1";
-courseDuration="0";
-course.save();
+
+router.get("/add",(req,res)=>{
+    res.render("add-course");
+})
+
+router.post("/add",(req,res)=>{
+    console.log(req.body);
+    var course=new CourseModel();
+    course.courseName=req.body.courseName;
+    course.courseFees=req.body.courseFees;
+    course.courseDuration=req.body.courseDuration;
+    course.Id= Math.ceil(Math.random()*10000000000);
+    course.save((err,doc)=>{
+    if(!err){
+        res.redirect("/course/list");
+    }
+    else {
+        res.send("Error occurred");
+    }
+});
+   
+})
+
+
 router.get("/list",(req, res)=>{
-    response.send("Course Controller)");
+
+
+
+
+    //response.send("Course Controller");
     CourseModel.find({},(err,docs)=>{
         if(!err){
             
